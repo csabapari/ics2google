631c4ae7677dbdc14f0f09a74c8a279c11b2c9de
xavi <csaba.pari@adnovum.hu>
2018-03-12 23:05:38 +0100

    Initial project setup
    Create initial project setup

---
+++ Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pari.ZimbraGoogle.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
